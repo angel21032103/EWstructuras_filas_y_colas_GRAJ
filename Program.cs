08c7d03ebcdc3e49ab8937e6181a6396e9645d9a
null <null>
2023-10-30 12:05:56 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace EWstructuras_filas_y_colas_GRAJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factorial(5));
+            Apilamiento();
+            Console.WriteLine();
+            Cola();
+            Console.ReadLine();
+
+        }
+
+        static int factorial(int N)
+        {
+            if (N == 0) { 
+                return 1;
+            }
+            return  N * factorial(N - 1);
+
+        
+        }
+
+        static void Apilamiento()
+        {
+            Stack<String> Nombres = new Stack<String>();
+            Nombres.Push("LinJoss");
+            Nombres.Push("Angel");
+            Nombres.Push("Leonardo");
+
+            int total = Nombres.Count;
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(" {0} ", Nombres.Pop());
+            }
+
+        }
+
+        static void Cola()
+        {
+            Queue<String> Fila = new Queue<String>();
+
+            Fila.Enqueue("LinJoss");
+            Fila.Enqueue("Angel");
+            Fila.Enqueue("Leonardo");
+
+            int total=Fila.Count;
+
+            for(int i = 0; i < total; i++)
+            {
+                Console.WriteLine("Turno de {0}",Fila.Dequeue());
+                Thread.Sleep(2000);
+
+            }
+
+
+
+
+        }
+
+
+    }
+}
